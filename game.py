010cbe3848c33ff6667346381a93c2f399ab3811
nateowl <55293968+nateowl@users.noreply.github.com>
2019-11-01 12:56:47 -0400

    Created basic Logic for the game and now can detect who wins

--- game.py
+++ game.py
@@ -1,33 +1,51 @@
-#import the random package so we can generate a random AI choicefrom random import randint
+#imports random package
 from random import randint
 
-#"basket" of choices
+#creates a basket or choices to choose from
 choices=["rock", "paper", "scissors"]
 
-# let the AI make a choice
+# let the computer ai make a choice
 computer=choices[randint(0,2)]
 
-#set up a game loop here so we dont have to keep restarting
+#set up a game loop here so we dont have to keep restarting it
 player= False
 
+#game loop
 while player is False:
-    player=input ("choose rock, paper or scissors:")
-    #print(computer, player)
+    player=input ("choose rock, paper or scissors: \n")
 
-    #start doing some logic and condition checking
+    #Checks to confirm it works
     print("computer:", computer, "player", player)
 
     #always check a breaking condition first
     if player == computer:
     #we have a tie, no point in going any further
-    print("tie, no one wins! try again")
+        print("Tie, no one wins! Try again")
     elif player == "quit":
-            print("you choose to quit, quitter.")
-            exit()
-    else:
-        print("Not a tie. oh snap")
-        if player == "rock":
-            print("check and see what the computer is, and win or lose")
+    #you quit, lets player exit game
+        print("You choose to quit, quitter.")
+        exit()
+
+    #Rock against (Paper or Scissors)
+    elif player == "rock":
+        if computer == "paper":
+            print("You lose!", computer, "covers", player, "\n")
+        else:
+            print("You won!", player, "smashes", computer, "\n")
+
+    #Paper against (Scissors or Rocks)
+    elif player == "paper":
+        if computer == "scissors":
+            print("You lose!", computer, "cuts", player, "\n")
+        else:
+            print("You won!", player, "covers", computer, "\n")
+
+    #Scissors against (Rock or paper)
+    elif player == "scissors":
+        if computer == "rock":
+            print("You lose!", computer, "smashes", player, "\n")
+        else:
+            print("You won!", player, "cuts", computer, "\n")
 
     player = False
     computer = choices[randint(0,2)]
